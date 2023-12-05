cb1b93bfc568d32a770dac8e2b900d39ac4780ac
jkotter1 <67186765+jkotter1@users.noreply.github.com>
2023-12-05 15:54:34 -0500

    working version of day2_2

---
+++ 2023/day2_2.py
@@ -0,0 +1,38 @@
+class Game:
+
+    def __init__(self, line, **kwargs):
+        result_str = line.split(":")[1].split(";")
+        
+        counts = [ [{color_cnt.split(" ")[2] : int(color_cnt.split(" ")[1])} for color_cnt in round.split(',')] for round in result_str]
+        
+        self.id = int(line.split("Game ")[1].split(":")[0])
+        
+        for count in counts:
+            for item in count:
+                color = list(item.keys())[0]
+                if item[color] > kwargs['mc'][color]:
+                    kwargs['mc'][color] = item[color]
+
+        self.power = 1
+        for x in list(kwargs['mc'].values()):
+            self.power * x 
+        
+
+
+if __name__ == "__main__":
+    with open('test_input2_1.txt', 'r') as file:
+        lines = [line.rstrip() for line in file]
+    
+    power_sum = 0
+    
+    max_colors = {
+        'red' : 0,
+        'green' : 0,
+        'blue' : 0
+    }
+    
+    for line in lines:
+        curr_game = Game(line, mc=max_colors)
+        power_sum += curr_game.id
+    
+    print(power_sum)
