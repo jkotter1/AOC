af6b5c73fac6b9cd2c2af9ede6c1c819f3baa3c2
jkotter1 <67186765+jkotter1@users.noreply.github.com>
2023-12-06 16:07:34 -0500

    new test input, day3_1 still not parsing all nums

--- 2023/day3_1.py
+++ 2023/day3_1.py
@@ -12,32 +12,35 @@ def is_part_num (lines, row, start_index, end_index):
     
     search_area = [line[start_search : end_search] for line in search_lines]
 
-    part_num = False
-
     for line in search_area:
         for char in line:
             if is_special_char(char):
-                part_num = True
+                return True
     
-    return part_num
+    return False
 
 if __name__ == "__main__":
-    with open('2023/input3_1.txt', 'r') as file:
+    with open('2023/test_input3_1.txt', 'r') as file:
         lines = [line.rstrip() for line in file]
 
     part_num_sum = 0
-
+    in_num = False
     for row, line in enumerate(lines):
-        in_num = False
+        
         for index, char in enumerate(line):
 
             if char.isdigit() and not in_num:
                 start_ind = index
                 in_num = True
 
-            elif in_num and not char.isdigit():
-                
-                if is_part_num(lines, row, start_ind, index-1):
+            elif in_num and (index == len(line) - 1 or not char.isdigit()):
+
+                if char.isdigit():
+                    end_ind = index
+                else:
+                    end_ind = index - 1
+
+                if is_part_num(lines, row, start_ind, end_ind):
                     print(line[start_ind : index])
                     part_num_sum += int(line[start_ind : index])
                 
