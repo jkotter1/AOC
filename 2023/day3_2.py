21b430be4e15a65f0b165a6fd3f735024574f77e
jkotter1 <67186765+jkotter1@users.noreply.github.com>
2023-12-07 15:32:52 -0500

    Added loop to find gear symbols

--- 2023/day3_2.py
+++ 2023/day3_2.py
@@ -1,24 +1,3 @@
-def is_special_char(char):
-    if char.isdigit() or char == '.':
-        return False
-    else:
-        return True
-    
-'''def is_part_num (lines, row, start_index, end_index):
-    
-    start_search = max(start_index - 1, 0)
-    end_search = min(end_index + 2, len(lines[row]))
-    search_lines = [lines[i] for i in range(max(row - 1, 0), min(row + 2, len(lines)))]
-    
-    search_area = [line[start_search : end_search] for line in search_lines]
-
-    for line in search_area:
-        for char in line:
-            if is_special_char(char):
-                    return True
-    
-    return False'''
-
 def find_nums(lines): #Returns a list for each line of the file with [start_index, end_index] lists for the locations of every distinct number (i.e. run of digits) 
     
     nums_by_line = []
@@ -49,38 +28,16 @@ def find_nums(lines): #Returns a list for each line of the file with [start_inde
 
     return nums_by_line
 
-
-
-
-
 if __name__ == "__main__":
     with open('2023/test_input3_1.txt', 'r') as file:
         lines = [line.rstrip() for line in file]
 
     num_list = find_nums(lines)
 
-    print(num_list)
-    '''part_num_sum = 0
-    in_num = False
-    for row, line in enumerate(lines):
-        
-        for index, char in enumerate(line):
+    for line in lines:
+        gear_count = 0
+        for char in line:
+            if char == '*':
+                gear_count +=1
+        print(gear_count)
 
-            if char.isdigit() and not in_num:
-                start_ind = index
-                in_num = True
-
-            elif in_num and (index == len(line) - 1 or not char.isdigit()):
-
-                if char.isdigit():
-                    end_ind = index
-                else:
-                    end_ind = index - 1
-
-                if is_part_num(lines, row, start_ind, end_ind):
-                    print(line[start_ind : end_ind + 1])
-                    part_num_sum += int(line[start_ind : end_ind + 1])
-                
-                in_num = False
-    
-    print(part_num_sum)'''
