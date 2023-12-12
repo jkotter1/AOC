5103a2d84d69b2f6f7bc417a52587c64897a68da
Joshua <joshua.kotter@gmail.com>
2023-12-12 00:04:22 -0500

    completed 4_1

--- 2023/day4_1.py
+++ 2023/day4_1.py
@@ -1,10 +1,19 @@
 if __name__ == "__main__":
-    with open('2023/test_input4.txt', 'r') as file:
+    with open('2023/input4.txt', 'r') as file:
         lines = [line.rstrip().split(':')[1].split('|') for line in file]
         
+        card_val_sum = 0
+
         for line in lines:
+            matches=0
             winners, owned = map(lambda x : x.split(), line)
 
-            winner_ints = [int(x) for x in winners]
-            owned_ints = [int(x) for x in owned]
-            pass
+            winner_ints = {int(x) for x in winners}
+            owned_ints = {int(x) for x in owned}
+            #print(winner_ints.intersection(owned_ints))
+            matches=len(winner_ints.intersection(owned_ints))
+            if matches>0:
+                card_value = 2 **  (matches-1)
+                print(card_value)
+                card_val_sum += card_value
+        print(card_val_sum)
