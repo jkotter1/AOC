b203f3d7e0467e0990b221fbc70c88cd71935479
jkotter1 <67186765+jkotter1@users.noreply.github.com>
2023-12-19 15:33:04 -0500

    added parsing for seeds and maps on day5_1

--- 2023/day5_1.py
+++ 2023/day5_1.py
@@ -1,4 +1,22 @@
+def int_list(int_str):
+    return [ int(x) for x  in int_str.strip().split(' ') ]
+
 if __name__ == "__main__":
     with open('2023/test_input5.txt', 'r') as file:
-        lines = [line.rstrip().split(':')[1] for line in file]
+        lines = [line.rstrip() for line in file]
+
+    seeds = int_list(lines[0].split(':')[1])
+
+    map_counter = -1
+    map_list = []
+
+    for line in lines[2:]:
+        if ':' in line:
+            map_list.append([])
+            map_counter += 1
+        elif line == '':
+            pass
+        else:
+            map_list[map_counter].append(int_list(line))
         
+    print(map_list)
