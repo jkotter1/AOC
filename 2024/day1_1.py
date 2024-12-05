5f703bb49eceb0d76b8f35bd43ec8c0295cc629b
Joshua <joshua.kotter@gmail.com>
2024-12-05 18:14:25 -0500

    complete day1_1

--- 2024/day1_1.py
+++ 2024/day1_1.py
@@ -1,4 +1,21 @@
 with open('input1.txt', 'r') as file:
     lines = [line.rstrip() for line in file]
 
-print(lines)
+#lines = ["3   4", "4   3", "2   5", "1   3", "3   9", "3   3"]
+
+list1, list2 = [], []  
+
+running_total = 0
+
+for line in lines:
+    int1, int2 = [int(i) for i in line.split()]
+    list1.append(int1)
+    list2.append(int2)
+
+list1.sort()
+list2.sort()
+
+for a, b in zip(list1, list2):
+    running_total += abs(a - b)
+
+print(running_total)
