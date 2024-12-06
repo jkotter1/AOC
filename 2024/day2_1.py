0cc4a91cf788d79748390d4bc6677c5feb82d07a
Joshua <joshua.kotter@gmail.com>
2024-12-05 22:51:44 -0500

    set up input 2 and initial day 2 soln file with text data

---
+++ 2024/day2_1.py
@@ -0,0 +1,6 @@
+with open('input2.txt', 'r') as file:
+    lines = [line.rstrip() for line in file]
+
+test = ["7 6 4 2 1","1 2 7 8 9","9 7 6 2 1","1 3 2 4 5","8 6 4 4 1","1 3 6 7 9"]
+
+print (test)
