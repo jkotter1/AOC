dd8e4c1ed6b51b873077c427d175e6ebafe534c8
Joshua <67186765+jkotter1@users.noreply.github.com>
2024-12-07 14:31:48 -0500

    Update day3_1.py
    Adding getInput func

--- 2024/day3_1.py
+++ 2024/day3_1.py
@@ -1,5 +1,12 @@
 import re
 
+def getInput():
+    with open('input3.txt', 'r') as file:
+        lines = [line.rstrip() for line in file]
+
+    #lines = ["7 6 4 2 1", "1 2 7 8 9", "9 7 6 2 1", "1 3 2 4 5", "8 6 4 4 1", "1 3 6 7 9"]
+    return lines
+
 def getDigits(line):
     line = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
