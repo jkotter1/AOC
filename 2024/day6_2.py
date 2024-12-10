68bc5aadd44872b31797749e7da6ed5e631c65b8
Joshua <joshua.kotter@gmail.com>
2024-12-09 22:36:51 -0500

    mod day6_2

--- 2024/day6_2.py
+++ 2024/day6_2.py
@@ -3,12 +3,10 @@ def getInput():
         labMap = [line.rstrip() for line in file]
     with open('testinput6.txt', 'r') as file:
         labMap = [line.rstrip() for line in file]
-
     return labMap
     
 def findStartAndBlocks(labMap):
     blockCoords = []
-    
     for rowInd, row in enumerate(labMap):
         for colInd, point in enumerate(row):
             if point == "^":
@@ -16,38 +14,7 @@ def findStartAndBlocks(labMap):
             elif point == "#":
                 blockCoords.append([rowInd, colInd])
     return startCoord, blockCoords
-    
-def moveGuard(currCoord, faceDir, blockCoords, locLog, labMap, **kwargs):
-    match faceDir:
-        case "up":
-            moveDir = [-1,0] 
-        case "down":
-            moveDir = [1,0] 
-        case "left":
-            moveDir = [0,-1] 
-        case "right":
-            moveDir = [0,1] 
-    
-    #newRow = list(labMap[currCoord[0]])
-    #newRow[currCoord[1]] = "X"
-    #labMap[currCoord[0]] = "".join(newRow)
 
-    # Element-wise addition
-    newCoord = [a + b for a, b in zip(currCoord, moveDir)]
-    
-    if checkLoop(newCoord, faceDir, locLog):
-        return {newCoord, faceDir, looped := False, exited := True
-    
-    elif checkExit(newCoord, len(labMap), len(labMap[0])):
-        return {newCoord, faceDir, looped := False, exited := True
-    
-    elif newCoord in blockCoords:
-        faceDir = turnGuard(faceDir)
-        return {currCoord, faceDir, looped := False, exited := False
-        
-    else:
-        return {newCoord, faceDir, False
-        
     
 def checkExit(coord, lenRow, lenCol):
     if -1 in coord: #off top or left of map
@@ -56,8 +23,6 @@ def checkExit(coord, lenRow, lenCol):
         return True
     else:
         return False
-
-
 
 def turnGuard(faceDir):
     match faceDir:
@@ -69,22 +34,54 @@ def turnGuard(faceDir):
             faceDir = "up"
         case "right":
             faceDir = "down" 
-            
     return faceDir
+
+def moveGuard(**kwargs):
+    currCoord = kwargs.get('currCoord')
+    faceDir = kwargs.get('faceDir')
+    blockCoords = kwargs.get('blockCoords')
+    locLog = kwargs.get('locLog')
+    labMap = kwargs.get('labMap')
+
+    moveDir = [None]
+    match faceDir:
+        case "up":
+            moveDir = [-1,0] 
+        case "down":
+            moveDir = [1,0] 
+        case "left":
+            moveDir = [0,-1] 
+        case "right":
+            moveDir = [0,1] 
+
+    # Element-wise addition
+    newCoord = [a + b for a, b in zip(currCoord, moveDir)]
     
+    if [newCoord, faceDir] in locLog: #if guard has been at this coord facing the same direction, guard has looped
+        return {'looped': True}
+    else:
+        locLog.append([newCoord, faceDir])
     
+    if checkExit(newCoord, len(labMap), len(labMap[0])):
+        return {'looped': False, 'exited': True}
     
+    elif newCoord in blockCoords: #if guard hits block, turn and don't progress
+        faceDir = turnGuard(faceDir)
+        newCoord = currCoord
+    
+    return {'currCoord': currCoord, 'faceDir': faceDir, 'blockCoords': blockCoords, 'locLog': locLog, 'labMap': labMap, 'looped': False, 'exited': False}
+
+
 if __name__ == "__main__":
     exited = False
     labMap = getInput()
     startCoord, blockCoords = findStartAndBlocks(labMap)
-    
-    
+
     #getInitialPath
-    initialRun = {'currCoord'=startCoord, 'faceDir' = "up", 'blockCoords'=blockCoords, 'locLog'=[], 'labMap'=labMap}
+    initialRun = {'currCoord':startCoord, 'faceDir': "up", 'blockCoords':blockCoords, 'locLog':[], 'labMap':labMap}
     while not exited:
         initialRun = moveGuard(**initialRun)
         
-    print()
+    print(initialRun['locLog'])
         
         
