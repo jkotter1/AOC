c9df6a1a5b96e7e4ab7549b753352fc688a5b1b0
Joshua <joshua.kotter@gmail.com>
2024-12-11 00:21:32 -0500

    soln too low on day7_1

--- 2024/day7_1.py
+++ 2024/day7_1.py
@@ -1,13 +1,13 @@
 import math
-from itertools import permutations 
+from more_itertools import distinct_permutations as idp
 
 def getInput():
     with open('input7.txt', 'r') as file:
         calib = [line.rstrip() for line in file]
     with open('testinput7.txt', 'r') as file:
-        calib = [line.rstrip() for line in file]
+       calib = [line.rstrip() for line in file]
     
-    calib = ["190: 10 19","3267: 81 40 27","83: 17 5","156: 15 6","7290: 6 8 6 15","161011: 16 10 13","192: 17 8 14","21037: 9 7 18 13","292: 11 6 16 20"]
+    #calib = ["190: 10 19","3267: 81 40 27","83: 17 5","156: 15 6","7290: 6 8 6 15","161011: 16 10 13","192: 17 8 14","21037: 9 7 18 13","292: 11 6 16 20"]
     
     cal = {}
     for line in calib:
@@ -17,25 +17,47 @@ def getInput():
     return cal
 
 def tryCalcs(cal):
-    calCount = 0
+    calSum = False
+
     
     for val in cal:
-        runningCalc = 0
-        if val = math.prod(cal[val]):
-                calCount += 1
-                break
+        cc = 1
+        print("Calculation "+str(cc)+" of "+str(len(cal)))
+        nums = cal[val]
+        if val == math.prod(nums):
+                calSum += val
                 
-        for ind, i in enumerate(cal[val]):
-            ops = ["+"]*ind + ["*"]*(len(cal[val])-1-ind)
-            
-            
-            else:
-                opPerms = permutations(ops) 
+        else:       
+            for ind, i in enumerate(nums):
+                opLog = []
+                stopCalcs = False
+                ops = ["*"]*ind + ["+"]*(len(nums)-1-ind)
+                opPerms = idp(ops) 
+                
                 for permInd, perm in enumerate(opPerms):
-                    for op in perm:
-                        if op =="+":
-                            
+                    if perm in opLog:
+                        continue
+                    else:
+                        opLog.append(perm)
 
+                    runningCalc = cal[val][0]
+                    
+                    for opInd, op in enumerate(perm):
+                        if op =="+":
+                            runningCalc += nums[opInd+1]
+                        elif op == "*":
+                            runningCalc *= nums[opInd+1]
+                    if runningCalc == val:
+                        calSum += val
+                        stopCalcs = True
+                        break
+                
+                if stopCalcs:
+                    break
+    return calSum
 
 if __name__ == "__main__":
     calib = getInput()
+    print(tryCalcs(calib))
+
+#20665830406676 is too low
