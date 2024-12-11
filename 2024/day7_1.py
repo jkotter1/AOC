ad9eb116b817f09948d1160a2bf27c9378282828
Joshua <67186765+jkotter1@users.noreply.github.com>
2024-12-10 22:54:05 -0500

    Update day7_1.py

--- 2024/day7_1.py
+++ 2024/day7_1.py
@@ -7,6 +7,8 @@ def getInput():
     with open('testinput7.txt', 'r') as file:
         calib = [line.rstrip() for line in file]
     
+    calib = ["190: 10 19","3267: 81 40 27","83: 17 5","156: 15 6","7290: 6 8 6 15","161011: 16 10 13","192: 17 8 14","21037: 9 7 18 13","292: 11 6 16 20"]
+    
     cal = {}
     for line in calib:
         nums = line.split(":")[1].split(" ")[1:]
@@ -15,16 +17,24 @@ def getInput():
     return cal
 
 def tryCalcs(cal):
-
+    calCount = 0
+    
     for val in cal:
+        runningCalc = 0
+        if val = math.prod(cal[val]):
+                calCount += 1
+                break
+                
         for ind, i in enumerate(cal[val]):
             ops = ["+"]*ind + ["*"]*(len(cal[val])-1-ind)
             
-        if val = math.prod(cal[val]):
-            calCount += 1
-        
-            perm = permutations([1, 2, 3]) 
-
+            
+            else:
+                opPerms = permutations(ops) 
+                for permInd, perm in enumerate(opPerms):
+                    for op in perm:
+                        if op =="+":
+                            
 
 
 if __name__ == "__main__":
