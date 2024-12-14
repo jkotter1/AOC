4d38a60543fdde631ed34b5b1aa8a8a70c93814c
Joshua <67186765+jkotter1@users.noreply.github.com>
2024-12-14 12:26:14 -0500

    Update day8_1.py
    Fix for counting too many antinodes.

--- 2024/day8_1.py
+++ 2024/day8_1.py
@@ -50,11 +50,11 @@ def calcAntinodes(coord1, coord2):
     x2 = coord2[0]
     y2 = coord2[1]
 
-    x3 = x1 - (x2 - x1)/2
-    x4 = x2 + (x2 - x1)/2
+    x3 = x1 - (x2 - x1)
+    x4 = x2 + (x2 - x1)
 
-    y3 = y1 - (y2 - y1)/2
-    y4 = y2 + (y2 - y1)/2
+    y3 = y1 - (y2 - y1)
+    y4 = y2 + (y2 - y1)
     
     return [(x3,y3), (x4,y4)]
 
